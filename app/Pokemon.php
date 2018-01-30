92ad74e91d1f545e6d09b784f5b8819fac89f879
Francis Goris Payano <fgpayano@gmail.com>
2018-01-30 17:52:40 -0400

    Fixed take damage messege

--- app/Pokemon.php
+++ app/Pokemon.php
@@ -12,52 +12,51 @@ class Pokemon
 	private $name = "";
 	private $attacks = [];
 	private $evolution = null;
-
 	const MIN_HP = 0;
 	const MAX_RESULTS = 1;
 
     public function __construct($name, array $attacks)
     {
-        $this->name = $name;
-        $this->attacks = $attacks;
-        $this->evolution = new MissingEvolution();
+		$this->name = $name;
+		$this->attacks = $attacks;
+		$this->evolution = new MissingEvolution();
     }
 
     public function setHp($hp)
     {
-        $this->hp = $hp;
+		$this->hp = $hp;
     }
 
     public function chooseAttack()
     {
-        $attack = array_rand($this->attacks, static::MAX_RESULTS);
+		$attack = array_rand($this->attacks, static::MAX_RESULTS);
 
-        return $this->attacks[$attack];
+		return $this->attacks[$attack];
     }
 
     public function getHp()
     {
-        return max($this->hp, static::MIN_HP);
+		return max($this->hp, static::MIN_HP);
     }
 
     public function setEvolution(Evolution $evolution = null)
     {
-        $this->evolution = $evolution;
+		$this->evolution = $evolution;
     }
 
     public function getName()
     {
-        return $this->name;
+		return $this->name;
     }
 
     public function dead()
     {
-        return $this->hp <= static::MIN_HP;
+		return $this->hp <= static::MIN_HP;
     }
 
     private function makeAttackFromEvolution(Attack $attack)
     {
-        return $this->evolution->powerUp($attack);
+		return $this->evolution->powerUp($attack);
     }
 
     public function takeDamage(Pokemon $attacker)
@@ -68,7 +67,7 @@ class Pokemon
 
         $this->setHp($this->getHp() - $attack->getPower());
 
-		Log::info("{$attacker->getName()} uso {$attacker->chooseAttack()->getName()}, {$this->getName()} tiene {$this->getHp()}/100 puntos de vida"); 
+		Log::info("{$attacker->getName()} uso {$attack->getName()}, {$this->getName()} tiene {$this->getHp()}/100 puntos de vida");
     }
 
     public function attack(Pokemon $opponent)
